da11978e8e234c6cc5baebec6711c3042b8bb927
EvgeniyKazancev <ekazancev777@gmail.com>
2023-05-04 22:07:07 +0300

    бот растет

--- src/main/java/ru/skillfacrtorydemo/tgbot/controllers/CurrencyController.java
+++ src/main/java/ru/skillfacrtorydemo/tgbot/controllers/CurrencyController.java
@@ -1,6 +1,7 @@
 package ru.skillfacrtorydemo.tgbot.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.skillfacrtorydemo.tgbot.dto.ValuteCursOnDate;
 import ru.skillfacrtorydemo.tgbot.service.CentralRussianBankService;
@@ -11,6 +12,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CurrencyController {
     private final CentralRussianBankService centralRussianBankService;
+    @GetMapping
     public List<ValuteCursOnDate> getValuteCursOnDate() throws Exception{
         return centralRussianBankService.getCurrenciesFromCbr();
     }
