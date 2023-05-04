33a9e0b7a8cd75f1421680765560d903fe8387fb
EvgeniyKazancev <ekazancev777@gmail.com>
2023-05-04 14:53:51 +0300

    бот растет

--- src/main/java/ru/skillfacrtorydemo/tgbot/dto/GetCursOnDateXmlResponse.java
+++ src/main/java/ru/skillfacrtorydemo/tgbot/dto/GetCursOnDateXmlResponse.java
@@ -1,9 +1,12 @@
 package ru.skillfacrtorydemo.tgbot.dto;
 
+import lombok.Data;
+
 import javax.xml.bind.annotation.*;
 
 @XmlRootElement(name = "GetCursOnDateXMLResponse", namespace = "http://web.cbr.ru/")
 @XmlAccessorType(XmlAccessType.FIELD)
+@Data
 public class GetCursOnDateXmlResponse {
 @XmlElement(name = "GetCursOnDateXMLResult", namespace = "http://web.cbr.ru/")
     private GetCursOnDateXmlResult getCursOnDateXmlResult;
